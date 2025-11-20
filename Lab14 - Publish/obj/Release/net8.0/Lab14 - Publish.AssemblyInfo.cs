6aa78eb3b4df4eb024af48b28b59b8b6fed01cd7
yVentura7 <168231053+yVentura7@users.noreply.github.com>
2025-11-19 22:31:55 -0500

    quinto commit

--- Lab14 - Publish/obj/Release/net8.0/Lab14 - Publish.AssemblyInfo.cs
+++ Lab14 - Publish/obj/Release/net8.0/Lab14 - Publish.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab14 - Publish")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbffcdc58fe78f4f919b4d7bbe1565d26cb8edf3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab14 - Publish")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab14 - Publish")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
